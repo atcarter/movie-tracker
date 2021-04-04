60deb47510505a3a6bd0b497cd110b7757b2ae91
Antonio <atcarter005@gmail.com>
2021-04-04 17:38:33 -0500

    Add validation for the username field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-    has_many :reviews
+  has_many :reviews
 
-    has_secure_password
-    
-    validates :email, presence: true, uniqueness: true
-    validates :password, presence: true
+  has_secure_password
+  
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
 end
