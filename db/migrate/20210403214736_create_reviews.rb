f237a2aac9964b7d5945dea54389bbb3e77185fd
Antonio <atcarter005@gmail.com>
2021-04-04 15:20:02 -0500

    Add documentation for foreign_key in review mgrtn

--- db/migrate/20210403214736_create_reviews.rb
+++ db/migrate/20210403214736_create_reviews.rb
@@ -5,6 +5,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
       t.integer :year
       t.integer :rating
       t.string :content
+      #foreign_key: true makes sure that a review is referencing an actual user record
       t.belongs_to :user, foreign_key: true
     end
   end
