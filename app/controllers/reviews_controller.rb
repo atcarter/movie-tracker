2b8a3234018b1990626c8551c34779a168aacf3d
Antonio <atcarter005@gmail.com>
2021-04-04 12:55:38 -0500

    Add functionality to create a new movie review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,10 +9,24 @@ class ReviewsController < ApplicationController
     end
   end
 
+  get '/reviews/new' do
+    @t = Time.now
+    erb :'/reviews/new'
+  end
+
   get '/reviews/:id' do
     id = params[:id].to_i
     @review = Review.find_by_id(id)
     erb :'/reviews/show'
+  end
+
+  post '/reviews' do
+    if is_logged_in?
+      @review = Review.create(title: params[:title], year: params[:year], rating: params[:rating], content: params[:content], user: current_user)
+      redirect "/reviews/#{@review.id}"
+    else
+      redirect "/"
+    end
   end
   
 end
